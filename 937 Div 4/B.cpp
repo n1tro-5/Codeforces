acf05f773b1827db531523ae257b51d8f8f16d30
NGUYỄN QUANG HUY <107240800+n1tro-5@users.noreply.github.com>
2024-04-04 00:02:10 +0700

    Create B.cpp

---
+++ 937 Div 4/B.cpp
@@ -0,0 +1,61 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define st first
+#define nd second
+#define pb push_back
+#define pf push_front
+#define _pb pop_back
+#define _pf pop_front
+#define lb lower_bound
+#define ub upper_bound
+#define ll long long
+#define lli long long int
+#define ull unsigned long long
+#define ldb long double
+#define db double
+#define str string
+#define WHILE while
+#define FOR for
+#define gcd __gcd
+#define lcm(a, b) (a) / gcd((a), (b)) * (b)
+
+#define oo (int)2e9 + 1
+#define PI 3.1415926535897932384626433832795l 
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ldb EPS = 1e-9;
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int n;
+    	cin >> n;
+
+    	for(int i = 0; i < 2*n; i++)
+    	{
+    		for(int j = 0; j < 2*n; j++)
+    		{
+    			if(((i/2) + (j/2)) %2 == 0)
+    				cout << '#';
+    			else
+    				cout << '.';
+    		}
+    		cout << endl;
+    	}
+    }
+    
+    return 0;
+}
+
+/* TEST CASE
+
+*/
